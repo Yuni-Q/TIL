2d04b04cefaa1ef8fc1f373bf8c1fb8436119c47
Yuni-Q <carnotcycle2@naver.com>
2018-12-15 20:09:39 +0900

    Add 알고리즘

---
+++ algorithm/javascript/9. Palindrome Number.js
@@ -0,0 +1,29 @@
+// Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+// Example 1:
+
+// Input: 121
+// Output: true
+// Example 2:
+
+// Input: -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  let a = JSON.stringify(x).split('').reverse().join('');
+  if(x == a){
+      return true;
+  } else {
+      return false;
+  }
+};
