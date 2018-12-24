0e49e1f7cf1207de8b14c47ed5f2d3fab5f15602
Yuni-Q <carnotcycle2@naver.com>
2018-12-24 10:05:49 +0900

    Fix 257

--- algorithm/javascript/257. Binary Tree Paths.js
+++ algorithm/javascript/257. Binary Tree Paths.js
@@ -27,33 +27,34 @@
  * @param {TreeNode} root
  * @return {string[]}
  */
-var binaryTreePaths = function(root) {
-  // console.log(root)
-  const paths = []
-  if(!root) return []
-  if(!root.left  && !root.right){
-      const val = JSON.stringify(root.val)
-      paths.push(val)
-      return paths
-  }
-  let stack = [root, root.val]
-  // console.log(stack.length)
-  while(stack.length>0){
-      let path = stack.pop();
-      let node = stack.pop();
-      // console.log(path,node)
-      if(!node.left  && !node.right){
-          paths.push(path)
-          console.log(path)
-      }
-      if(node.left){
-          stack.push(node.left)
-          stack.push(`${path}->${node.left.val}`)
-      }
-      if(node.right){
-          stack.push(node.right)
-          stack.push(`${path}->${node.right.val}`)
-      }
-  }
-  return paths.reverse()
+var binaryTreePaths = function (root) {
+    // console.log(root)
+    const paths = []
+    if (!root) return []
+    if (!root.left && !root.right) {
+        const val = JSON.stringify(root.val)
+        paths.push(val)
+        return paths
+    }
+    let stack = [root, root.val]
+    // console.log(stack.length)
+    while (stack.length > 0) {
+        let path = stack.pop();
+        let node = stack.pop();
+        // console.log(path,node)
+        if (!node.left && !node.right) {
+            paths.push(path)
+            console.log(path)
+        }
+
+        if (node.right) {
+            stack.push(node.right)
+            stack.push(`${path}->${node.right.val}`)
+        }
+        if (node.left) {
+            stack.push(node.left)
+            stack.push(`${path}->${node.left.val}`)
+        }
+    }
+    return paths
 };
