901177a11769c797ad3c2f69339961601e7b927a
Yuni-Q <carnotcycle2@naver.com>
2018-12-24 10:27:17 +0900

    Fix 6

--- algorithm/javascript/6. ZigZag Conversion.js
+++ algorithm/javascript/6. ZigZag Conversion.js
@@ -28,37 +28,39 @@
  * @param {number} numRows
  * @return {string}
  */
-var convert = function(s, numRows) {
-  let j = 0
-  let k = 0
-  let result = []
-  let result2 = "";
-  for(let i = 0 ; i < s.length; i+=1){
-      if(!k){
-          if(!result[j]) result[j] = ""    
-          result[j] += s[i]
-          j += 1;
-      } else {
-          result[k] += s[i]
-          k -= 1;
-      }
-      
-      if(j === numRows){
-          if(numRows < 2){
-              k = 0;
-              j = 0;
-          } else {
-              k = numRows-2;
-              j = 0;
-          }
-      }   
-  }
-  console.log(result)
-  for(let i = 0 ; i < numRows; i+=1){
-      if(!result[i]) result[i] = "" 
-      result2 += result[i]
-  }
-  
-  return result2;
-  
+var convert = function (s, numRows) {
+    let j = 0
+    let k = 0
+    let result = []
+    let result2 = "";
+    for (let i = 0; i < s.length; i += 1) {
+        if (!k) {
+            if (!result[j]) result[j] = ""
+            result[j] += s[i]
+            j += 1;
+        } else {
+            result[k] += s[i]
+            k -= 1;
+        }
+
+        if (j === numRows) {
+            if (numRows < 2) {
+                k = 0;
+                j = 0;
+            } else {
+                k = numRows - 2;
+                j = 0;
+            }
+        }
+    }
+    console.log(result)
+    for (let i = 0; i < numRows; i += 1) {
+        if (!result[i]) result[i] = ""
+        result2 += result[i]
+    }
+
+    return result2;
+
 };
+
+// numRows로 잘라서 접어서 계산하면 마지막에 result를 위한 loop를 사용하지 않아서 s의 length만큼만 반복합니다.
