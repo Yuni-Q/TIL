7e335bc276fce3a4c48aefbe99b13aec8098fbc2
Yuni-Q <carnotcycle2@naver.com>
2019-01-02 00:16:10 +0900

    Add 448

---
+++ algorithm/javascript/448. Find All Numbers Disappeared in an Array.js
@@ -0,0 +1,34 @@
+
+// Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+// Find all the elements of [1, n] inclusive that do not appear in this array.
+
+// Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+// Example:
+
+// Input:
+// [4,3,2,7,8,2,3,1]
+
+// Output:
+// [5,6]
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+  const result = {};
+  const result2 = []
+  for(let i = 0; i <nums.length; i+=1){
+      result[nums[i]] = true;
+  }
+  const max = Math.max.apply(null, nums);
+  // console.log(result)
+  for(let i = 1; i <= nums.length; i+=1){
+      if(result[i] !== true){
+          result2.push(i)
+      }
+  }
+  return result2  
+};
