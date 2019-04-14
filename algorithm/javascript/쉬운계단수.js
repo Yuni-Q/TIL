4340e7c6606609accac077fedd73c4aa33804f3f
Yuni-Q <carnotcycle2@naver.com>
2019-04-14 10:07:23 +0900

    알고리즘 쉬운계단수

---
+++ algorithm/javascript/쉬운계단수.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin', 'utf8').split('\n');
+var idx = 0;
+var n =  parseInt(input[idx++]);
+var check = 0;
+var mod = 1e9;
+var DT = [];
+DT[1] = [0,1,1,1,1,1,1,1,1,1];
+for (var i = 2; i <= n; i++) {
+  if(!DT[i])  DT[i] = [];
+  for (var j = 0; j < 10; j++) {
+    DT[i][j] = 0;
+    if (j+1 <= 9)  DT[i][j] += DT[i-1][j+1];
+    if (j-1 >= 0) DT[i][j] += DT[i-1][j-1];
+    DT[i][j] %= mod;
+  }
+}
+
+var ans = DT[n].reduce(function (a,b) {
+  return a+b;
+});
+console.log(ans%mod);
