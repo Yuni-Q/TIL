3365f7fea1dc665668164b58b582b8754b255f8b
Yuni-Q <carnotcycle2@naver.com>
2019-04-26 14:36:33 +0900

    디자인 패턴 과제 1

---
+++ architecture/디자인패턴/과제1.ts
@@ -0,0 +1,57 @@
+interface Charater {
+  fight();
+  setWeapon: (w: WeaponBehavior) => void;
+}
+
+class Character implements Charater {
+  weapon = null;
+
+  constructor(w: WeaponBehavior) {
+    this.weapon = w;
+  }
+  
+  setWeapon = (w: WeaponBehavior) => {
+    this.weapon = w;
+  }
+
+  fight = () => {}
+}
+
+class Queen extends Character {
+  // fight = () => {}
+}
+class King extends Character {
+  // fight = () => {}
+}
+
+class Troll extends Character {
+  // fight = () => {}
+}
+
+interface WeaponBehavior {
+  userWeapon: () => void;
+}
+
+class KnifeBehavior {
+  userWeapon = () => {
+    // 칼로 베는 것을 구현
+  }
+}
+
+class AxeBehavior implements WeaponBehavior {
+  userWeapon = () => {
+    // 도끼로 베는 것을 구현
+  }
+} 
+
+class SwordBehavior implements WeaponBehavior {
+  userWeapon = () => {
+    // 검을 휘두르는 것을 구현
+  }
+}
+
+class BowAndAroowBehavior implements WeaponBehavior {
+  userWeapon = () => {
+    // 화를 써서 화살을 발사하는 것을 구현
+  }
+}
