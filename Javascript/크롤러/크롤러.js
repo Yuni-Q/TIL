1d69873c7e85691e93328a87315c6a647f28bbec
Yuni-Q <carnotcycle2@naver.com>
2019-05-03 22:55:03 +0900

    크롤러

--- Javascript/크롤러/크롤러.js
+++ Javascript/크롤러/크롤러.js
@@ -4,8 +4,8 @@ const cheerio = require('cheerio');
 async function getText () {
   const html = await axios.get('https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=brooklyn-nine-nine&episode=s01e01')
   const $ = cheerio.load(html.data);
-
-  const text = await $("#content_container > div.main-content > div.main-content-left > div.episode_script div.scrolling-script-container").text()
+  const text = $("#content_container > div.main-content > div.main-content-left > div.episode_script div.scrolling-script-container")
+    .text()
   console.log(text);
 }
 
