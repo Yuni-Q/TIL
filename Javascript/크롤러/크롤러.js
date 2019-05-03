8201141c4c6ace75134cb9442cb4a88d921d99a2
Yuni-Q <carnotcycle2@naver.com>
2019-05-03 22:45:30 +0900

    크롤러

---
+++ Javascript/크롤러/크롤러.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function getText () {
+  const html = await axios.get('https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=brooklyn-nine-nine&episode=s01e01')
+  const $ = cheerio.load(html.data);
+
+  const text = await $("#content_container > div.main-content > div.main-content-left > div.episode_script div.scrolling-script-container").text()
+  console.log(text);
+}
+
+getText();
+
+
+// axios.get('https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=brooklyn-nine-nine&episode=s01e01')
+//   .then((html) => {
+//     return cheerio.load(html.data)
+//   })
+//   .then(($) => {
+//     return $("#content_container > div.main-content > div.main-content-left > div.episode_script div.scrolling-script-container").text()
+//   })
+//   .then((text) => {
+//     console.log(text);
+//   })
